cecb488496c3e8ef926a48c7502092980e3a30a1
0x27F6 <bad.shade.blue@gmail.com>
2026-04-02 19:27:05 -0400

    added comment clarifying the purpose of the inner join as a filter as opposed to a left join

--- polymarket_dbt/models/marts/fct_markets.sql
+++ polymarket_dbt/models/marts/fct_markets.sql
@@ -23,6 +23,7 @@ joined as (
         t.unique_traders,
         m.tags
     from metadata m
+    -- inner join intentionally excludes condition_ids below threshold on split NegRisk outcomes
     join trades t
         on m.unique_key = t.unique_key
         and m.condition_id = t.condition_id 
