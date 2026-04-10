c859690bea8026b4767cfead26a3959982fb9c3f
shade <106406249+0x27F6@users.noreply.github.com>
2026-04-10 10:40:21 -0400

    Update  join_completeness.sql
    adjusted dependency

--- dune_sql/checks/metadata/ join_completeness.sql
+++ dune_sql/checks/metadata/ join_completeness.sql
@@ -2,7 +2,7 @@
 
 /*
 Test: Metadata Join Completeness
-Dependency: dune.com/queries 
+Dependency: https://dune.com/queries/6932485 
 
 Expectation:
 All qualified markets (>= $10k scoped volume) must have corresponding metadata.
