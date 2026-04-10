d3cf1bb76612991be745cd5e427236aeeae3933a
shade <106406249+0x27F6@users.noreply.github.com>
2026-04-10 12:38:32 -0400

    Create non_negative_enforcement.sql

---
+++ dune_sql/checks/trades/non_negative_enforcement.sql
@@ -0,0 +1,47 @@
+-- Dune Query: https://dune.com/queries/6979343
+
+/*
+Purpose: Check to see if any negative value trades occurred in our 
+observed universe(should be impossible)
+
+Expected outcome: Return no rows 
+
+Note: Dune query linked above, can be run as is.
+
+*/
+
+with 
+
+scoped_trades as (
+    select 
+        cast(unique_key as varchar) as unique_key,
+        amount
+    from polymarket_polygon.market_trades
+    where taker in (
+        0x4bfb41d5b3570defd03c39a9a4d8de6bd8b8982e,
+        0xc5d563a36ae78145c45a50134d48a1215220f80a
+    )
+),
+
+market_stats as (
+    select 
+        unique_key,
+        sum(amount) as market_volume
+    from scoped_trades
+    group by unique_key
+),
+
+qualified_markets as (
+    select unique_key
+    from market_stats
+    where market_volume >= 10000
+)
+
+select
+    st.unique_key,
+    st.tx_hash,
+    st.amount
+from scoped_trades st
+join qualified_markets qm
+    on st.unique_key = qm.unique_key
+where st.amount < 0
